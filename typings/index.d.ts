f62a517ef28cf02a6cb9164411d8197776b63fc6
Yudan Lin <yudan.lin@hpe.com>
2016-08-15 17:35:24 +0800

    ...

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/bootstrap/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
+/// <reference path="globals/uuid/index.d.ts" />
